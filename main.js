e1c81ab77334515dc51fa886e6795d2d178d9c00
Ezequiel <ezmartgod@gmail.com>
2023-05-06 14:35:21 -0300

    entrega 3

--- main.js
+++ main.js
@@ -1,96 +1,135 @@
-let productos = [];
+let productos;
+initialize();
 
-function Product(id, title, price, stock, description) {
-  this.id = id;
-  this.title = title;
-  this.price = price;
-  this.stock = stock;
-  this.description = description;
+function getProductos() {
+  const localProducts = localStorage.getItem('products');
+  return JSON.parse(localProducts);
 }
 
-function findId() {
-  let idFlag = true;
-  while (idFlag === true) {
-    const id = prompt('Ingrese ID del producto');
-    console.log(typeof id);
-    console.log(typeof parseInt(id));
-    let match = productos.filter(prod => prod.id === id);
-    if (match.length > 0) {
-      alert('Otro producto ya tiene esa ID, debes probar con otra...');
-    } else {
-      return id;
-    }
+function setProductos(array) {
+  const productsToAdd = JSON.stringify(array);
+  localStorage.setItem('products', productsToAdd);
+}
+
+function renderProductos() {
+  const newProducts = getProductos();
+  newProducts.sort((a, b) => a.id - b.id);
+
+  const productosContainer = document.getElementById('productos');
+  productosContainer.innerHTML = '';
+  let html = newProducts.map(prod => {
+    const tr = document.createElement('tr');
+    const content = `
+    <td>${prod.id}</td>
+    <td>${prod.title}</td>
+    <td>${prod.stock}</td>
+    <td>${prod.price}</td>
+    <td>${prod.description}</td>`;
+    tr.innerHTML = content;
+    return tr;
+  });
+  html.forEach(elm => {
+    productosContainer.appendChild(elm);
+  });
+}
+
+function initialize() {
+  const localProducts = localStorage.getItem('products');
+
+  if (localProducts === null) {
+    localStorage.setItem('products', JSON.stringify([]));
+  } else {
+    productos = JSON.parse(localProducts);
+  }
+
+  renderProductos();
+}
+
+class Product {
+  constructor(id, title, price, stock, description) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.stock = stock;
+    this.description = description;
   }
 }
 
-function createProduct() {
-  let id = findId();
-  let title = prompt('Ingrese nombre del producto');
-  let price = prompt('Ingrese precio del producto');
-  let stock = prompt('Ingrese stock del producto');
-  let description = prompt('Ingrese descripcion del producto');
-
-  let producto = new Product(id, title, price, stock, description);
-  productos.push(producto);
-  App();
+function findId(id) {
+  const newProducts = getProductos();
+  let match = newProducts.filter(prod => prod.id === parseInt(id));
+  if (match.length > 0) {
+    alert('Otro producto ya tiene esa ID, debes probar con otra...');
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function deleteProduct() {
-  let allProducts = '';
-  for (let i = 0; i < productos.length; i++) {
-    let product = `Producto ${productos[i].id}: ${productos[i].title}\n`;
-    allProducts += product;
+function FormHandler(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  if (findId(data.id)) {
+    let producto = new Product(parseInt(data.id), data.title, data.price, data.stock, data.description);
+    const newProducts = getProductos();
+    newProducts.push(producto);
+    setProductos(newProducts);
+    alert('Producto agregado exitosamente.');
+    renderProductos();
   }
-  if (productos.length !== 0) {
-    let option = prompt('Elija la ID de un producto para eliminar:\n' + allProducts);
-    if (productos.find(prod => prod.id === option)) {
-      let nuevosProductos = productos.filter(prod => prod.id !== option);
-      if (nuevosProductos.length > 0) {
-        productos = nuevosProductos;
-        alert('Se eliminó el producto con id ' + option);
-        App();
-      }
-    } else {
-      alert('No existe un producto con esa ID, por favor, intente nuevamente');
-      deleteProduct();
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', function (e) {
+  FormHandler(e);
+});
+
+function DeleteHandler(e) {
+  e.preventDefault();
+  const deleteId = document.getElementById('deleteID').value;
+  const newProducts = getProductos();
+  const index = newProducts.findIndex(prod => prod.id === parseInt(deleteId));
+  if (index > -1) {
+    let selection = confirm(`Estas seguro que queres borrar el producto ${productos[index].title} (con ID ${deleteId})?`);
+    if (selection) {
+      alert('Producto eliminado correctamente');
+      newProducts.splice(index, 1);
+      setProductos(newProducts);
+      renderProductos();
     }
   } else {
-    alert('Aun no se han agregado productos!');
-    App();
+    alert(`No existe un producto con ID ${deleteId}`);
   }
 }
 
-function showProductos() {
-  let allProducts = '';
-  for (let i = 0; i < productos.length; i++) {
-    let product = `Producto ${productos[i].id}:
-    Titulo: ${productos[i].title}
-    Precio: $ ${productos[i].price}
-    Stock: ${productos[i].stock}
-    Description: ${productos[i].description}\n`;
-    allProducts += product + '\n';
-  }
-  if (productos.length !== 0) {
-    alert(allProducts);
+const deleteForm = document.getElementById('deleteForm');
+deleteForm.addEventListener('submit', function (e) {
+  DeleteHandler(e);
+});
+
+function StockHandler(e) {
+  e.preventDefault();
+  const modifyId = document.getElementById('modifyID').value;
+  const newProducts = getProductos();
+  const index = newProducts.findIndex(prod => prod.id === parseInt(modifyId));
+  if (index > -1) {
+    let newStock = prompt(`Elija nuevo stock para ${newProducts[index].title}\nStock actual: ${newProducts[index].stock}`);
+    let intNewStock = parseInt(newStock);
+    if (parseInt(intNewStock) > 0) {
+      alert(`Stock modificado correctamente, nuevo stock es ${newStock}`);
+      newProducts[index].stock = newStock;
+      setProductos(newProducts);
+      renderProductos();
+    } else {
+      alert('Ese stock no es valido, reintente');
+    }
   } else {
-    alert('Aun no se han agregado productos!');
-  }
-  App();
-}
-
-function App() {
-  let option = prompt('CONTROL DE STOCK\nOpciones:\n1) Agregar productos\n2) Eliminar un producto\n3) Ver los productos');
-  switch (option) {
-    case '1':
-      createProduct();
-    case '2':
-      deleteProduct();
-    case '3':
-      showProductos();
-    default:
-      alert('Esa opcion no es valida');
-      App();
+    alert(`No existe un producto con ID ${modifyId}`);
   }
 }
 
-App();
+const modifyForm = document.getElementById('modifyForm');
+modifyForm.addEventListener('submit', function (e) {
+  StockHandler(e);
+});
